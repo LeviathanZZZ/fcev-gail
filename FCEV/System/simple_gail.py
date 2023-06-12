eefbf1f0e831dc024c16a3a44760557e2fb42aac
LeviathanZZ <1051818528@qq.com>
2023-06-12 17:01:08 +0800

    比较稳定的GAIL，添加了BC

--- FCEV/System/simple_gail.py
+++ FCEV/System/simple_gail.py
@@ -3,8 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import gym
+from tqdm import tqdm
 import time
-
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader, Dataset
+Writer = SummaryWriter()
 #####################  hyper parameters  ####################
 EPISODES = 200
 EP_STEPS = 200
@@ -17,11 +20,57 @@ BATCH_SIZE = 256
 RENDER = False
 ENV_NAME = 'Pendulum-v1'
 
+
+class MyDataset(Dataset):
+    def __init__(self, file_path):
+        dataset = np.load(file_path, allow_pickle=True).item()
+
+        self.observations = torch.from_numpy(dataset['observations'])
+        self.actions = torch.from_numpy(dataset['actions'])
+
+    def __getitem__(self, idx):
+        return self.observations[idx], self.actions[idx]
+
+    def __len__(self):
+        return len(self.observations)
+
+
+class BehavioralCloning(nn.Module):
+    def __init__(self, n_states, n_actions, lr):
+        super(BehavioralCloning, self).__init__()
+
+        self.fc1 = nn.Linear(n_states, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, n_actions)
+
+        self.lr = lr
+
+        self.lossfn = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=0.0001)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        output = 2 * F.tanh(x)
+
+        return output
+
+    def choose_action(self, x):
+        x = torch.FloatTensor(x)
+        # x = x.to(torch.float32)
+        action = self.forward(x)
+        return action.detach().numpy()
+
 def sample_expert_data(ddpg, n_episode):
     states = []
     actions = []
     env = gym.make('Pendulum-v1')
-    for episode in range(n_episode):
+    dataset = {"observations": [], "actions": []}
+    idx = 0
+    while True:
         state = env.reset()
         done = False
         total_rewards = 0
@@ -30,11 +79,21 @@ def sample_expert_data(ddpg, n_episode):
             a = ddpg.choose_action(state)
             next_state, reward, done, _ = env.step(a)
             states.append(state)
+            dataset['observations'].append(state)
+            dataset['actions'].append(a)
             actions.append(a)
             total_rewards += reward
             state = next_state
+            idx += 1
+            if idx > 200 * n_episode:
+                dataset["observations"] = np.array(dataset["observations"]).reshape(-1, 3)
+                dataset["actions"] = np.array(dataset["actions"]).reshape(-1, 1)
+                np.save('datasets10000.npy', dataset)
+                break
+        if idx > 200 * n_episode:
+            break
         print("Score：", total_rewards)
-        env.close()
+
     return np.array(states), np.array(actions)
 
 ########################## DDPG Framework ######################
@@ -43,11 +102,15 @@ class ActorNet(nn.Module):  # define the network structure for actor and critic
         super(ActorNet, self).__init__()
         self.fc1 = nn.Linear(s_dim, 128)
         self.fc1.weight.data.normal_(0, 0.1)  # initialization of FC1
+        self.fc2 = nn.Linear(128, 128)
+        self.fc2.weight.data.normal_(0, 0.1)  # initialization of FC1
         self.out = nn.Linear(128, a_dim)
         self.out.weight.data.normal_(0, 0.1)  # initilizaiton of OUT
 
     def forward(self, x):
         x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
         x = F.relu(x)
         x = self.out(x)
         x = torch.tanh(x)
@@ -62,13 +125,16 @@ class CriticNet(nn.Module):
         self.fcs.weight.data.normal_(0, 0.1)
         self.fca = nn.Linear(a_dim, 128)
         self.fca.weight.data.normal_(0, 0.1)
+        self.fc1 = nn.Linear(128, 128)
+        self.fc1.weight.data.normal_(0, 0.1)
         self.out = nn.Linear(128, 1)
         self.out.weight.data.normal_(0, 0.1)
 
     def forward(self, s, a):
         x = self.fcs(s)
         y = self.fca(a)
-        actions_value = self.out(F.relu(x + y))
+        actions_value = self.fc1(F.relu(x + y))
+        actions_value = self.out(F.relu(actions_value))
         return actions_value
 
 class Discriminator(nn.Module):
@@ -243,6 +309,7 @@ def run_ddpg():
             ep_r += r
             if j == EP_STEPS - 1:
                 print('Episode: ', i, ' Reward: %i' % (ep_r), 'Explore: %.2f' % var)
+                Writer.add_scalar(tag="return",scalar_value=ep_r,global_step=i)
                 # if ep_r > -300: RENDER = True
                 break
     print('Running time: ', time.time() - t1)
@@ -282,10 +349,125 @@ def run_gail():
             ep_r += r
             if j == EP_STEPS - 1:
                 print('Episode: ', i, ' Reward: %i' % (ep_r), 'Explore: %.2f' % var)
+                Writer.add_scalar(tag="return", scalar_value=ep_r, global_step=i)
                 # if ep_r > -300: RENDER = True
                 break
     print('Running time: ', time.time() - t1)
 
+def run_bc():
+    env = gym.make(ENV_NAME)
+    env = env.unwrapped
+    env.seed(1)
+    s_dim = env.observation_space.shape[0]
+    a_dim = env.action_space.shape[0]
+    a_bound = env.action_space.high
+    a_low_bound = env.action_space.low
+
+    ddpg = DDPG(a_dim, s_dim, a_bound)
+    ddpg.actor_eval.load_state_dict(torch.load(r'.\ddpg_actor.ckpt'))
+    expert_s,expert_a = sample_expert_data(ddpg,1)
+    var = 3  # the controller of exploration which will decay during training process
+    t1 = time.time()
+    model = BehavioralCloning(s_dim, a_dim, lr=0.001)
+    file_path = 'datasets10000.npy'
+    mydataset = MyDataset(file_path)
+    # mydataset = MyDataset(expert_s,expert_a)
+    train_loader = DataLoader(dataset=mydataset, batch_size=16, shuffle=True)
+    for epoch in range(4):
+        for i, data in enumerate(tqdm(train_loader)):
+            states, actions = data
+            states = states.to(torch.float32)
+            actions = actions.to(torch.float32)
+            model.optimizer.zero_grad()
+            pre_actions = model.forward(states)
+            loss = model.lossfn(pre_actions, actions)
+            loss.backward()
+            model.optimizer.step()
+
+            if i % 10 == 9:
+                print('epoch: {} , i: {}, loss: {}'.format(epoch, i, loss))
+
+    torch.save(model.state_dict(), r'.\bc.ckpt')
+
+    print('Running time: ', time.time() - t1)
+
+def test_bc():
+    env = gym.make(ENV_NAME)
+    env = env.unwrapped
+    env.seed(1)
+    s_dim = env.observation_space.shape[0]
+    a_dim = env.action_space.shape[0]
+    a_bound = env.action_space.high
+    a_low_bound = env.action_space.low
+    model = BehavioralCloning(s_dim,a_dim,lr=0.001)
+    model.load_state_dict(torch.load(r'.\bc.ckpt'))
+    for episode in range(100):
+        state = env.reset()
+        done = False
+        total_rewards = 0
+        for i in range(200):
+            # env.render()
+            action = model.choose_action(state)
+            next_state, reward, done, _ = env.step(action)
+            total_rewards += reward
+            state = next_state
+        print("Score：", total_rewards)
+        Writer.add_scalar(tag="eval_return", scalar_value=total_rewards, global_step=episode)
+        env.close()
+
+def test_DDPG():
+    env = gym.make(ENV_NAME)
+    env = env.unwrapped
+    env.seed(1)
+    s_dim = env.observation_space.shape[0]
+    a_dim = env.action_space.shape[0]
+    a_bound = env.action_space.high
+    a_low_bound = env.action_space.low
+    ddpg = DDPG(a_dim,s_dim,a_bound)
+    ddpg.actor_eval.load_state_dict(torch.load(r'.\ddpg_actor.ckpt'))
+    for episode in range(100):
+        state = env.reset()
+        done = False
+        total_rewards = 0
+        for i in range(200):
+            # env.render()
+            action = ddpg.choose_action(state)
+            next_state, reward, done, _ = env.step(action)
+            total_rewards += reward
+            state = next_state
+        print("Score：", total_rewards)
+        Writer.add_scalar(tag="eval_return", scalar_value=total_rewards, global_step=episode)
+        env.close()
+
+def test_GAIL():
+    env = gym.make(ENV_NAME)
+    env = env.unwrapped
+    env.seed(1)
+    s_dim = env.observation_space.shape[0]
+    a_dim = env.action_space.shape[0]
+    a_bound = env.action_space.high
+    a_low_bound = env.action_space.low
+    ddpg = DDPG(a_dim,s_dim,a_bound)
+    ddpg.actor_eval.load_state_dict(torch.load(r'.\gail_actor.ckpt'))
+    for episode in range(100):
+        state = env.reset()
+        done = False
+        total_rewards = 0
+        for i in range(200):
+            # env.render()
+            action = ddpg.choose_action(state)
+            next_state, reward, done, _ = env.step(action)
+            total_rewards += reward
+            state = next_state
+        print("Score：", total_rewards)
+        Writer.add_scalar(tag="eval_return",scalar_value=total_rewards,global_step=episode)
+        env.close()
+
+
 if __name__ == '__main__':
     # run_ddpg()
-    run_gail()
+    # run_gail()
+    # run_bc()
+    # test_DDPG()
+    # test_GAIL()
+    test_bc()
